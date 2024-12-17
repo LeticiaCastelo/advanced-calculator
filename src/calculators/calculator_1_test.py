aadab600e723e7d5e063330b3b71445c88fd46c6
Letícia Castelo <leticiacastelo.dev@gmail.com>
2024-12-17 10:34:23 -0300

    versão final do projeto

--- src/calculators/calculator_1_test.py
+++ src/calculators/calculator_1_test.py
@@ -1,4 +1,5 @@
 from typing import Dict
+from pytest import raises
 from .calculator_1 import Calculator1
 
 class MockRequest:
@@ -12,6 +13,7 @@ def test_calculate():
     response = calculator_1.calculate(mock_request)
     print()
     print(response)
+
     assert "data" in response
     assert "Calculator" in response["data"]
     assert "result" in response["data"]
@@ -21,4 +23,10 @@ def test_calculate():
     assert response["data"]["Calculator"] == 1
 
 def test_calculate_with_body_error():
-    pass
+    mock_request = MockRequest(body={"something": 1})
+    calculator_1 = Calculator1()
+
+    with raises(Exception) as excinfo:
+        calculator_1.calculate(mock_request)
+
+    assert str(excinfo.value) == "body mal formatado!"
